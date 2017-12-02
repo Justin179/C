80298fbc7f8c849a81af3e4cfe8fc8a823500dae
Justin-Chen <justin.uob@gmail.com>
2017-12-02 12:04:28 +0800

    lunch

--- Lab18/main18.c
+++ Lab18/main18.c
@@ -8,7 +8,11 @@
 #include <stdio.h>
 int main(int argc, char **argv) {
 
+	int items[] = {3,1,4,1,5,9,2,6,8,37,45};
+	printf("the first element is %d,%d\n",items[0],*items); // *items¬O«ü Index 0
+	printf("the third element is %d,%d\n",items[2],*(items+2));
 
+	printf("items & *items %d,%d\n",items,*items); // -13440,3
 
 	return 0;
 }
