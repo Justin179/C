8a2af4815058a9775710cf478036d561433b924e
Justin-Chen <justin.uob@gmail.com>
2017-12-16 10:07:44 +0800

    size of char short int long

---
+++ Lab33/main33.c
@@ -0,0 +1,18 @@
+/*
+ * main33.c
+ *
+ *  Created on: 2017¦~12¤ë16¤é
+ *      Author: Administrator
+ */
+
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+int main(int argc, char **argv) {
+	printf("char size = %lu\n",sizeof(char));
+	printf("short size = %lu\n",sizeof(short));
+	printf("int size = %lu\n",sizeof(int));
+	printf("long size = %lu\n",sizeof(long));
+	return 0;
+}
+
