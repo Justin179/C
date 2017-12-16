7e83ba6c4d74972eb641e02b39cd9b6e0bda8d47
Justin-Chen <justin.uob@gmail.com>
2017-12-16 10:18:35 +0800

    各數值的範圍區間

--- Lab33/main33.c
+++ Lab33/main33.c
@@ -13,6 +13,19 @@ int main(int argc, char **argv) {
 	printf("short size = %lu\n",sizeof(short));
 	printf("int size = %lu\n",sizeof(int));
 	printf("long size = %lu\n",sizeof(long));
+
+	printf("\nlong long size = %lu\n",sizeof(long long));
+	printf("float size = %lu\n",sizeof(float));
+	printf("double size = %lu\n",sizeof(double));
+
+	printf("\nchar max is %i, min is %i \n",CHAR_MAX, CHAR_MIN);
+	printf("SHRT_MAX is %i, SHRT_MIN is %i \n",SHRT_MAX, SHRT_MIN);
+	printf("INT_MAX is %i, INT_MIN is %i \n",INT_MAX, INT_MIN);
+	printf("LONG_MAX is %li, LONG_MIN is %li \n",LONG_MAX, LONG_MIN);
+	printf("FLT_MAX is %f, FLT_MIN is %f \n",FLT_MAX, FLT_MIN);
+	printf("DBL_MAX is %f, DBL_MIN is %f \n",DBL_MAX, DBL_MIN);
+
+
 	return 0;
 }
 
