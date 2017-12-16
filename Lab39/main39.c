0c55c44aa95659b1df887e1e7aee57ede932dbc8
Justin-Chen <justin.uob@gmail.com>
2017-12-16 16:54:27 +0800

    sorting 2

--- Lab39/main39.c
+++ Lab39/main39.c
@@ -8,15 +8,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// class
 typedef struct asset {
 	char* name;
 	int twd;
 	int usd;
 } asset;
+// four objs
 asset John = {"John",100,100};
 asset Mark = {"Mark",80,100};
 asset Tim = {"Tim",100,80};
 asset Mary = {"Mary",80,80};
+// 實作比較方法
 int compareAsset(const void* assetA, const void* assetB){
 	asset* compare1 = (asset*) assetA;
 	asset* compare2 = (asset*) assetB;
@@ -32,7 +35,9 @@ int compareAsset(const void* assetA, const void* assetB){
 	return z - z2;
 }
 int main(int argc, char **argv) {
-	asset members[] = {John,Mark,Tim,Mary};
+	// 把4個物件放到一個容器
+	asset members[] = {John,Mark,Tim,Mary}; // 將被排序的物件
+	// void *array, size_t count, size_t size, comparison_fn_t compare
 	qsort(members,4,sizeof(asset),compareAsset); // 排序
 	printf("ranking orders:\n");
 	for(int i = 0; i<4; i++){
