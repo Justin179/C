c58e9e6e594ab005036dd089270d276326fc7058
Justin-Chen <justin.uob@gmail.com>
2018-01-06 13:50:13 +0800

    class 69

--- Lab69/main69.cpp
+++ Lab69/main69.cpp
@@ -20,19 +20,26 @@ public:
 		y = j;
 	}
 	friend ostream& operator <<(ostream& stream, coord obj); // 類似介面的東西
+	friend istream& operator >>(istream& stream, coord& obj);
 };
 
 ostream& operator <<(ostream& stream, coord obj){
 	stream << obj.x << ", " << obj.y << endl;
 	return stream;
 }
-
+istream& operator >>(istream& stream, coord& obj){
+	cout << "enter coordinate: ";
+	stream >> obj.x >> obj.y;
+	return stream;
+}
 
 
 int main(int argc, char **argv) {
-
 	coord a(1,1), b(5,7), c;
 	cout << a << b << c << endl;
+
+	cin >> c;
+	cout << c;
 
 	return 0;
 }
